faf4e1b21387735c2085735c775af7fe9e0d3b68
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 17:59:48 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      i++
+    end
+
+  else
+    return 'A block was not passed.'
+  end
 end
