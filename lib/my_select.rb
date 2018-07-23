3a1e60634471a374bd7b7c3b96281f1663061923
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 18:05:35 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    newArray = []
+
+    while i < collection.length
+      if yield(collection[i])
+        newArray.push(collection[i])
+      end
+      i += 1
+    end
+
+    newArray
+  else
+    return 'A block was not passed.'
+  end
 end
