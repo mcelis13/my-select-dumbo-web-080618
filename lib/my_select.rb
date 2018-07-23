f2d09d85c819e752beee0185e1108a8d2ad9b290
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 18:00:43 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,13 @@
 def my_select(collection)
   if block_given?
     i = 0
-
+    newArray = []
+    
     while i < collection.length
       i++
     end
-
+    
+    newArray
   else
     return 'A block was not passed.'
   end
